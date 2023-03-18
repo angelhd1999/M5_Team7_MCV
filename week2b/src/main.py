f819716daa02baae319a2ef0ce8fac56d4702238
angelhd1999 <angelhd1999@gmail.com>
2023-03-18 13:58:20 +0100

    MaskRCNN + Training

--- week2b/src/main.py
+++ week2b/src/main.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import json
 from PIL import Image
 import cv2
 import numpy as np
@@ -8,13 +9,14 @@ import pycocotools.mask as cocomask
 from detectron2 import model_zoo
 from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_test_loader
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-from detectron2.engine import DefaultPredictor #, DefaultTrainer
+from detectron2.engine import DefaultPredictor, DefaultTrainer
 from detectron2.config import get_cfg
 from detectron2.structures.boxes import BoxMode
 from detectron2.utils.visualizer import Visualizer
 
-DATASET_PATH = '../../../mcv/datasets/KITTI-MOTS'
+DATASET_PATH = '../../../KITTI-MOTS'
 MODEL = 'FasterRCNN' # 'MaskRCNN' or 'FasterRCNN'
+FINETUNING = True
 N_IMAGES_TEST = 10
 N_WORKERS = 8
 
@@ -36,12 +38,16 @@ for i in range(80):
 
 print("Starting script")
 
-def get_kitti_mots_dicts(data_path, mode):
+# Enble CUDA
+print("Enabling CUDA")
+torch.cuda.set_device(0)
+
+def get_kitti_mots_dicts(data_path, mode, model):
     # assert mode in ['training', 'testing'], "mode should be 'training' or 'testing'"
     
     image_path = os.path.join(data_path, mode, 'image_02')
     instance_path = os.path.join(data_path, 'instances')
-    instance_txt_path = os.path.join(data_path, 'instances_txt')
+    instance_txt_path = os.path.join(data_path, 'instances_txt' + '_' + mode)
 
     dataset_dicts = []
 
@@ -73,14 +79,15 @@ def get_kitti_mots_dicts(data_path, mode):
                 frame_id, class_instance_ids, class_id_re, in_height, in_width, rle_encoding = values
                 
                 frame_id = int(frame_id)
-
+                in_height = int(in_height)
+                in_width = int(in_width)
                 # Parse class and instance id
                 class_id = int(class_instance_ids) // 1000
                 class_id_re = int(class_id_re) # ? It's redundant, but we'll keep it for now
                 instance_id = int(class_instance_ids) % 1000
                 
                 # Decode RLE mask encoding
-                rle = {'counts': rle_encoding, 'size': [int(in_height), int(in_width)]}
+                rle = {'counts': rle_encoding, 'size': [in_height, in_width]}
                 binary_mask = cocomask.decode(rle)
 
                 if class_id_re > 2 or np.sum(binary_mask) == 0:
@@ -88,8 +95,8 @@ def get_kitti_mots_dicts(data_path, mode):
                     
                 # print("Found object with class id " + str(class_id) + " and instance id " + str(instance_id) + " in frame " + str(frame_id))
                 # Compute the bounding box from the mask
-                y_indices, x_indices = np.where(binary_mask)
-                bbox = [np.min(x_indices), np.min(y_indices), np.max(x_indices) - np.min(x_indices), np.max(y_indices) - np.min(y_indices)]
+                y_indices, x_indices = np.where(binary_mask == 1)
+                bbox = [int(np.min(x_indices)), int(np.min(y_indices)), int(np.max(x_indices) - np.min(x_indices)), int(np.max(y_indices) - np.min(y_indices))]
                 
                 # Map class id to COCO class id
                 class_id = class_mapping_k_to_c[class_id]
@@ -99,9 +106,13 @@ def get_kitti_mots_dicts(data_path, mode):
                     "bbox_mode": BoxMode.XYWH_ABS,
                     "category_id": class_id,
                     "instance_id": instance_id,
-                    "segmentation": rle,
                 }
                 objs.append(obj)
+
+                if (model == 'MaskRCNN'):
+                    shape = [(x_indices, y_indices) for x_indices, y_indices in zip(x_indices, y_indices)]
+                    shape = [s for x in shape for s in x]
+                    obj["segmentation"] = shape
 
             record["annotations"] = objs
             # record["sem_seg_file_name"] = inst_path # ? Not sure if we need this
@@ -110,13 +121,39 @@ def get_kitti_mots_dicts(data_path, mode):
     return dataset_dicts
 
 
-def setup_config(model_name):
+def setup_config_predef(model_name):
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file(model_name))
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_name)
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5 # Set threshold for this model
     cfg.DATASETS.TEST = ("kitti_mots_testing",)
     cfg.MODEL.FP16_ENABLED = True # Enable mixed precision training for faster inference
+    cfg.MODEL.DEVICE='cuda'
+    return cfg
+
+def setup_config_finetuning(model_name, train_dataset_name, val_dataset_name):
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(model_name))
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_name)
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # Set threshold for this model
+    cfg.DATASETS.TRAIN = (train_dataset_name,)
+    cfg.DATASETS.VAL = (val_dataset_name,)
+    cfg.DATASETS.TEST = ()
+    cfg.MODEL.FP16_ENABLED = True  # Enable mixed precision training for faster inference
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128  # You can adjust this value depending on your GPU memory
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # Number of classes in KITTI-MOTS dataset (excluding the ignore class)
+    cfg.MODEL.DEVICE='cuda'
+
+    # Set up the training parameters
+    cfg.SOLVER.IMS_PER_BATCH = 4
+    cfg.SOLVER.BASE_LR = 0.001
+    cfg.SOLVER.MAX_ITER = 1000  # You can adjust the number of iterations based on your needs
+    cfg.SOLVER.STEPS = []  # Do not decay learning rate
+    cfg.SOLVER.GAMMA = 0.05
+    cfg.SOLVER.CHECKPOINT_PERIOD = 500  # Save a checkpoint every 500 iterations
+    cfg.TEST.EVAL_PERIOD = 500  # Evaluate the model every 500 iterations
+
+
     return cfg
 
 def run_inference(predictor, image_list):
@@ -150,21 +187,37 @@ def visualize_predictions(predictions, image_list, metadata, output_dir):
 
 print("Loading KITTI-MOTS dataset")
 # Register KITTI-MOTS dataset
-for d in ["training", "testing"]:
+for d in ["train_gen", "val_gen"]:
     print("Registering KITTI-MOTS " + d + " dataset")
-    DatasetCatalog.register("kitti_mots_" + d, lambda d=d: get_kitti_mots_dicts(DATASET_PATH, d) )
+    DatasetCatalog.register("kitti_mots_" + d, lambda d=d: get_kitti_mots_dicts(DATASET_PATH, d, MODEL) )
     MetadataCatalog.get("kitti_mots_" + d).set(thing_classes=["Car", "Pedestrian", "Ignore"])
 
 # Get the metadata for the KITTI-MOTS training dataset, used for visualizing predictions
-kitti_mots_metadata = MetadataCatalog.get("kitti_mots_training")
+kitti_mots_metadata = MetadataCatalog.get("kitti_mots_val_gen")
 
 print(f"Loading models and weights from {MODEL} model") 
 if MODEL == 'FasterRCNN':
-    cfg = setup_config("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml") # Some options: "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml" / "COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"
+    if FINETUNING:
+        cfg = setup_config_finetuning("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml", "kitti_mots_train_gen", "kitti_mots_val_gen")
+    else:
+        cfg = setup_config_predef("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml") # Some options: "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml" / "COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"
 elif MODEL == 'MaskRCNN':
-    cfg = setup_config("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml") # Some options: "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml" / "COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"
+    if FINETUNING:
+        cfg = setup_config_finetuning("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml", "kitti_mots_train_gen", "kitti_mots_val_gen")
+    else:
+        cfg = setup_config_predef("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml") # Some options: "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml" / "COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"
 else:
     raise ValueError("MODEL should be 'MaskRCNN' or 'FasterRCNN'")
+
+
+if FINETUNING:
+    # Create a DefaultTrainer
+    trainer = DefaultTrainer(cfg)
+    trainer.resume_or_load(resume=False)
+
+    # Train the model
+    trainer.train()
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
 
 predictor = DefaultPredictor(cfg)
 
@@ -181,8 +234,8 @@ visualize_predictions(predictions, shortened_test_image_list, kitti_mots_metadat
 print("Visualization done")
 
 print("Evaluating predictions")
-evaluator = COCOEvaluator("kitti_mots_testing", cfg, False, output_dir="./output/")
-val_loader = build_detection_test_loader(cfg, "kitti_mots_testing", num_workers=N_WORKERS)
+evaluator = COCOEvaluator("kitti_mots_val_gen", cfg, False, output_dir="./output/")
+val_loader = build_detection_test_loader(cfg, "kitti_mots_val_gen", num_workers=N_WORKERS)
 evaluation_results = inference_on_dataset(predictor.model, val_loader, evaluator)
 print("Evaluation done")
 
