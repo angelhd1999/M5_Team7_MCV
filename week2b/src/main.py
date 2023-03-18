1b95ad5f9f90d5f1200dc10b01594050bf4b858a
angelhd1999 <angelhd1999@gmail.com>
2023-03-18 01:53:18 +0100

    COCOEvaluation & improved code

--- week2b/src/main.py
+++ week2b/src/main.py
@@ -6,8 +6,8 @@ import numpy as np
 import torch
 import pycocotools.mask as cocomask
 from detectron2 import model_zoo
-from detectron2.data import DatasetCatalog, MetadataCatalog #, build_detection_test_loader
-# from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_test_loader
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
 from detectron2.engine import DefaultPredictor #, DefaultTrainer
 from detectron2.config import get_cfg
 from detectron2.structures.boxes import BoxMode
@@ -16,6 +16,23 @@ from detectron2.utils.visualizer import Visualizer
 DATASET_PATH = '../../../mcv/datasets/KITTI-MOTS'
 MODEL = 'FasterRCNN' # 'MaskRCNN' or 'FasterRCNN'
 N_IMAGES_TEST = 10
+N_WORKERS = 8
+
+class_mapping_k_to_c = {  # Mapping KITTI-MOTS class indices to COCO class indices
+    1: 2, # KITTI-MOTS class 1 is "car" when read, so we map it to COCO class 2
+    2: 1 # KITTI-MOTS class 2 is "person" when read, so we map it to COCO class 1
+}
+
+class_mapping_c_to_k = {  # Mapping COCO class indices to KITTI-MOTS class indices
+    2: 0, # COCO class 2 is "car" so we map it to KITTI-MOTS class 0 (defined at thing_classes)
+    1: 1 # COCO class 1 is "person" so we map it to KITTI-MOTS class 1 (defined at thing_classes)
+} 
+ignore_class = 2
+
+# Add all other COCO classes to the mapping dictionary as an "Ignore" class
+for i in range(80):
+    if i not in class_mapping_c_to_k:
+        class_mapping_c_to_k[i] = ignore_class
 
 print("Starting script")
 
@@ -25,7 +42,7 @@ def get_kitti_mots_dicts(data_path, mode):
     image_path = os.path.join(data_path, mode, 'image_02')
     instance_path = os.path.join(data_path, 'instances')
     instance_txt_path = os.path.join(data_path, 'instances_txt')
-    
+
     dataset_dicts = []
 
     for seq in sorted(os.listdir(image_path)):
@@ -33,13 +50,13 @@ def get_kitti_mots_dicts(data_path, mode):
         instances = sorted(glob.glob(os.path.join(instance_path, seq, '*.png')))
         instance_txt = os.path.join(instance_txt_path, seq + '.txt')
         
+        # Log progress
+        print("Loading " + str(len(images)) + " images from sequence " + seq)
+
         with open(instance_txt, 'r') as f:
             instance_data = f.readlines()
 
         for idx, (img_path, inst_path) in enumerate(zip(images, instances)):
-            # Log progress
-            if idx % 100 == 0:
-                print("Processing sequence " + seq + ", image " + str(idx) + "/" + str(len(images)))
             record = {}
             img = Image.open(img_path)
             width, height = img.size
@@ -69,11 +86,14 @@ def get_kitti_mots_dicts(data_path, mode):
                 if class_id_re > 2 or np.sum(binary_mask) == 0:
                     continue
                     
-                print("Found object with class id " + str(class_id) + " and instance id " + str(instance_id) + " in frame " + str(frame_id))
+                # print("Found object with class id " + str(class_id) + " and instance id " + str(instance_id) + " in frame " + str(frame_id))
                 # Compute the bounding box from the mask
                 y_indices, x_indices = np.where(binary_mask)
                 bbox = [np.min(x_indices), np.min(y_indices), np.max(x_indices) - np.min(x_indices), np.max(y_indices) - np.min(y_indices)]
                 
+                # Map class id to COCO class id
+                class_id = class_mapping_k_to_c[class_id]
+
                 obj = {
                     "bbox": bbox,
                     "bbox_mode": BoxMode.XYWH_ABS,
@@ -96,6 +116,7 @@ def setup_config(model_name):
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_name)
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5 # Set threshold for this model
     cfg.DATASETS.TEST = ("kitti_mots_testing",)
+    cfg.MODEL.FP16_ENABLED = True # Enable mixed precision training for faster inference
     return cfg
 
 def run_inference(predictor, image_list):
@@ -112,16 +133,6 @@ def run_inference(predictor, image_list):
 
 def visualize_predictions(predictions, image_list, metadata, output_dir):
     os.makedirs(output_dir, exist_ok=True)
-    class_mapping = {  # Mapping COCO class indices to KITTI-MOTS class indices
-        3: 0, # COCO class 3 is "car" so we map it to KITTI-MOTS class 0
-        1: 1 # COCO class 1 is "person" so we map it to KITTI-MOTS class 1
-    } 
-    ignore_class = 2
-
-    # Add all other COCO classes to the mapping dictionary as an "Ignore" class
-    for i in range(80):
-        if i not in class_mapping:
-            class_mapping[i] = ignore_class
 
     for i, (pred, img_path) in enumerate(zip(predictions, image_list)):
         print(f"Visualizing prediction {i + 1}/{len(image_list)}: {img_path}")
@@ -129,7 +140,7 @@ def visualize_predictions(predictions, image_list, metadata, output_dir):
         visualizer = Visualizer(img[:, :, ::-1], metadata=metadata, scale=1.0)
         
         instances = pred["instances"].to("cpu")
-        instances.pred_classes = torch.tensor([class_mapping[c.item()] for c in instances.pred_classes])  # Apply the class mapping
+        instances.pred_classes = torch.tensor([class_mapping_c_to_k[c.item()] for c in instances.pred_classes])  # Apply the class mapping
         filtered_instances = instances[instances.pred_classes != ignore_class]  # Filter out the "ignore" class
         vis_output = visualizer.draw_instance_predictions(filtered_instances)
         
@@ -149,9 +160,9 @@ kitti_mots_metadata = MetadataCatalog.get("kitti_mots_training")
 
 print(f"Loading models and weights from {MODEL} model") 
 if MODEL == 'FasterRCNN':
-    cfg = setup_config("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
+    cfg = setup_config("COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml") # Some options: "COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml" / "COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x.yaml"
 elif MODEL == 'MaskRCNN':
-    cfg = setup_config("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    cfg = setup_config("COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml") # Some options: "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml" / "COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml"
 else:
     raise ValueError("MODEL should be 'MaskRCNN' or 'FasterRCNN'")
 
@@ -168,3 +179,14 @@ print("Inference done")
 print("Visualizing predictions")
 visualize_predictions(predictions, shortened_test_image_list, kitti_mots_metadata, "output_visualizations")
 print("Visualization done")
+
+print("Evaluating predictions")
+evaluator = COCOEvaluator("kitti_mots_testing", cfg, False, output_dir="./output/")
+val_loader = build_detection_test_loader(cfg, "kitti_mots_testing", num_workers=N_WORKERS)
+evaluation_results = inference_on_dataset(predictor.model, val_loader, evaluator)
+print("Evaluation done")
+
+# Print the evaluation results
+print("Evaluation Results:")
+for key, value in evaluation_results.items():
+    print(f"{key}: {value}")
